dbe21b2234d562ed93fd6d91b89ffda170102291
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-03-18 09:14:10 +0100

    feat: functions

--- investment_calculator.go
+++ investment_calculator.go
@@ -12,10 +12,12 @@ func main() {
 	var years float64
 	expectedReturnedRate := 5.5
 
-	fmt.Print("investment amount: ")
+	// fmt.Print("Investment amount: ")
+	outputText("Investment amount: ")
 	fmt.Scan(&investmentAmount)
 
-	fmt.Print("Expected Returned Rate: ")
+	// fmt.Print("Expected Returned Rate: ")
+	outputText("Expected Returned Rate: ")
 	fmt.Scan(&expectedReturnedRate)
 
 	fmt.Print("Years: ")
@@ -24,6 +26,19 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnedRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
-	fmt.Println(futureValue)
-	fmt.Println(futureRealValue)
+	formattedFV := fmt.Sprintf("future value: %.1f\n", futureValue)
+	formattedRFV := fmt.Sprintf("Future Value (adjusted for inflation): %.1f\n", futureRealValue)
+
+	// output information
+	// fmt.Println("Future Value:", futureValue)
+	// fmt.Printf(`Future Value: %.1f
+	// Future Value (adjusted for inflation): %.1f`, futureValue, futureRealValue)
+
+	// fmt.Println("Future Value (adjusted for inflation):", futureRealValue)
+
+	fmt.Print(formattedFV, formattedRFV)
+}
+
+func outputText(text string) {
+	fmt.Print(text)
 }
